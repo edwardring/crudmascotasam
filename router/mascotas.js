1410f75ea171b2b5e355a36ed58df35ee5a757d3
edwardring <edward.rodriguez@gmail.com>
2024-07-29 07:51:53 -0500

    Vista de mascotas dinamica

--- router/mascotas.js
+++ router/mascotas.js
@@ -2,7 +2,12 @@ const express = require('express')
 const router = express.Router();
 
 router.get("/", (req, res)=>{
-    res.render('mascotas', {titulo: "Página de Mascotas"});
+    res.render('mascotas', { arrayMascotas: [
+        {id: 'mas001', nombre: 'Rex', descripcion: 'Perro Pastor Aleman'},
+        {id: 'mas002', nombre: 'Piolin', descripcion: 'Canario Cantor'},
+        {id: 'mas003', nombre: 'Trueno', descripcion: 'Gato Persa'},
+        {id: 'mas004', nombre: 'Tabata', descripcion: 'Perrita callejera'}
+    ]});
 });
 
 module.exports = router;
