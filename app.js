693dfc1a8a625a3cac8e31c41d23caad200fe17a
edwardring <edward.rodriguez@gmail.com>
2024-08-02 07:56:21 -0500

    CRUD inicial mascotas

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 /* conexion a la base de datos mongodb cloud */
 const mongoose = require('mongoose');
--- router/mascotas.js
+++ router/mascotas.js
@@ -13,4 +13,63 @@ router.get("/", async (req, res)=>{
     }
 });
 
+router.get("/crear", (req, res)=>{
+    res.render('crear');
+});
+
+/* router para recibir datos del formulario crear */
+router.post("/", async (req, res)=>{
+    const body = req.body;
+    // console.log(body);
+    try {
+        await Mascota.create(body)
+        res.redirect('/mascotas')
+    } catch (error) {
+        console.log('error: ', error)
+    }
+});
+
+/* router para editar un documento */
+router.get("/:id", async (req, res)=>{
+    const id = req.params.id;
+    try {
+        const mascotaDB = await Mascota.findOne({_id: id })
+        // console.log(mascotaDB)
+        res.render('detalle',{
+            mascota: mascotaDB,
+            error: false
+        })
+    } catch (error) {
+        console.log('error: ', error)
+        res.render('detalle',{
+            error: true,
+            mensaje: "No se encontró ningún registro qué coincida con el id"
+        })
+    }
+});
+
+/* router para borrar un documento */
+router.delete("/:id", async (req, res)=>{
+    const id = req.params.id;
+    try {
+        const mascotaDB = await Mascota.findByIdAndDelete({ _id: id })
+        if (!mascotaDB) {
+            res.json({
+                estado: false,
+                mensaje: "No fue posible eliminar el registro"
+            })
+            
+        } else {
+            res.json({
+                estado: true,
+                mensaje: "Registro eliminado!!!"
+            })
+        }
+
+    } catch (error) {
+        console.log('error: ', error)
+
+    }
+});
+
 module.exports = router;
